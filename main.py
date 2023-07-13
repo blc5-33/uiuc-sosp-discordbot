cf3fafd90dbf60f1898fd811d66fcd5d86b802d7
mastersurvival5 <54222922+mastersurvival5@users.noreply.github.com>
2023-07-13 13:52:47 -0500

    Added server online checks, added fail-connect msg

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ from mcstatus import JavaServer
 import discord
 from discord.ext import commands
 
-load_dotenv()
-
 # Type aliases
 Context = commands.Context
 
@@ -16,12 +14,29 @@ class MinecraftBot(commands.Bot):
                  **kwargs):
         super().__init__(**kwargs)
         self.SERVER_UNSET_MESSAGE = "No Minecraft server has been set yet!"
+        self.SERVER_CONNECT_FAIL_MESSAGE = "Could not connect to that address's Minecraft server!"
         self.minecraft_java_server = None
+    
+    def server_is_set(self) -> bool:
+        return self.minecraft_java_server
 
-    def current_server_ip(self) -> str:
-        if self.minecraft_java_server:
-            address = self.minecraft_java_server.address
-            return f"{address.host.split('_tcp.')[-1]}:{address.port}"
+    def server_is_online(self) -> bool:
+        if self.server_is_set():
+            try:
+                self.minecraft_java_server.status()
+            except:
+                return False
+            return True
+        
+        return False
+    
+    def server_ip(self) -> str:
+        """
+        Returns the IP address\n
+        MUST check `server_is_online()` before calling this function.
+        """
+        address = self.minecraft_java_server.address
+        return f"{address.host.split('_tcp.')[-1]}:{address.port}"
 
 
 # Intents - what the bot is meant to do
@@ -37,30 +52,35 @@ bot = MinecraftBot(command_prefix="/mc ", intents=intents)
 # - check Discord's documentation for more info
 @bot.command()
 async def setserver(ctx: Context, mcServerName: str):
+    prev = bot.minecraft_java_server
     bot.minecraft_java_server = JavaServer.lookup(mcServerName)
-    try:
-        bot.minecraft_java_server.status()
-    except:
-        await ctx.send(f"Could not connect to that address's Minecraft server!")
+    if not bot.server_is_online():
+        await ctx.send(bot.SERVER_CONNECT_FAIL_MESSAGE)
+        bot.minecraft_java_server = prev
         return
-    await ctx.send(f"Set associated Minecraft server to `{bot.current_server_ip()}`")
+    await ctx.send(f"Set associated Minecraft server to `{bot.server_ip()}`")
 
 @bot.command()
 async def getserver(ctx: Context):
-    if bot.minecraft_java_server:
-        await ctx.send(f"Associated Minecraft server IP is `{bot.current_server_ip()}`")
-    else:
+    if not bot.server_is_set():
         await ctx.send(bot.SERVER_UNSET_MESSAGE)
+    elif bot.server_is_online():
+        await ctx.send(f"Associated Minecraft server IP is `{bot.server_ip()}`")
+    else:
+        await ctx.send(bot.SERVER_CONNECT_FAIL_MESSAGE)
 
 @bot.command()
 async def online(ctx: Context):
-    if bot.minecraft_java_server:
-        status = bot.minecraft_java_server.status()
-        await ctx.send(f"There are **{status.players.online}/{status.players.max}** players online on `{bot.current_server_ip()}`")
-    else:
+    if not bot.server_is_set():
         await ctx.send(bot.SERVER_UNSET_MESSAGE)
+    elif bot.server_is_online():
+        status = bot.minecraft_java_server.status()
+        await ctx.send(f"There are **{status.players.online}/{status.players.max}** players online on `{bot.server_ip()}`")
+    else:
+        await ctx.send(bot.SERVER_CONNECT_FAIL_MESSAGE)
 
 
+load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 bot.run(BOT_TOKEN)
 
