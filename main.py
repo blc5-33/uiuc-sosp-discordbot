76b500f98bb60ffeeaeda8ee3f6f5318d192a3da
mastersurvival5 <54222922+mastersurvival5@users.noreply.github.com>
2023-07-17 14:45:46 -0500

    All server status requests are embeds + bot icon

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import discord
 from discord.ext import commands
 import minecraftbot
 from mcstatus import JavaServer
+import base64
 
-from typing import Self
+import logging
 
 Context = commands.Context
 
@@ -23,51 +24,66 @@ async def setserver(ctx: Context, mcServerName: str):
     prev = bot.minecraft_java_server
     bot.minecraft_java_server = JavaServer.lookup(mcServerName)
     if not bot.server_is_online():
-        await ctx.send(bot.SERVER_CONNECT_FAIL_MESSAGE)
+        await ctx.send(embed=bot.SERVER_CONNECT_FAIL_EMBED)
         bot.minecraft_java_server = prev
         return
-    await ctx.send(f"Set associated Minecraft server to `{bot.server_ip()}`")
+
+    await ctx.send(file=bot.update_icon('server-icon.png'),
+                   embed=bot.template_embed(
+                    title=f"Set associated Minecraft server to `{bot.server_ip()}`") \
+                    .set_thumbnail(url="attachment://server-icon.png"))
+    
 
 @bot.command()
 async def getserver(ctx: Context):
     if not bot.server_is_set():
-        await ctx.send(bot.SERVER_UNSET_MESSAGE)
+        await ctx.send(embed=bot.SERVER_UNSET_EMBED)
     elif bot.server_is_online():
-        await ctx.send(f"Associated Minecraft server IP is `{bot.server_ip()}`")
+        await ctx.send(file=bot.update_icon('server-icon.png'),
+                   embed=bot.template_embed(
+                    title=f"Associated Minecraft server `{bot.server_ip()}`") \
+                    .set_thumbnail(url="attachment://server-icon.png"))
     else:
-        await ctx.send(bot.SERVER_CONNECT_FAIL_MESSAGE)
+        await ctx.send(embed=bot.SERVER_CONNECT_FAIL_EMBED)
 
 @bot.command()
 async def online(ctx: Context):
     if not bot.server_is_set():
-        await ctx.send(bot.SERVER_UNSET_MESSAGE)
+        await ctx.send(embed=bot.SERVER_UNSET_EMBED)
     elif bot.server_is_online():
         status = bot.minecraft_java_server.status()
-        await ctx.send(f"""There are **{status.players.online}/{status.players.max}** players 
-                       online on `{bot.server_ip()}`""")
+        await ctx.send(file=bot.update_icon('server-icon.png'),
+                   embed=bot.template_embed(
+                    title=f"There are **{status.players.online}/{status.players.max}** players "
+                          f"online on `{bot.server_ip()}`") \
+                    .set_thumbnail(url="attachment://server-icon.png"))
     else:
-        await ctx.send(bot.SERVER_CONNECT_FAIL_MESSAGE)
+        await ctx.send(embed=bot.SERVER_CONNECT_FAIL_EMBED)
 
 import planview
-from datetime import datetime
 
 @bot.command()
 async def plan(ctx: Context, action: str, *tokens):
     await ctx.message.delete()
     if action == "create":
         message = " ".join(tokens)
-        embed = discord.Embed(color=discord.Color.green(), title=message, timestamp=datetime.now()) \
-                .set_footer(text="Minecraft Looking For Group Bot") \
+        embed = bot.template_embed(title=message) \
                 .set_author(name=ctx.author.display_name, icon_url=ctx.author.display_avatar) \
-                .set_thumbnail(url="https://static.wikia.nocookie.net/minecraft_gamepedia/images/a/a4/Grass_Block_%28item%29_BE5.png/revision/latest?cb=20200901112517") \
-                .add_field(name="Interested Players:", value="No interested players yet... Be the first!", inline=False)
+                .add_field(name="Interested Players:", 
+                           value="No interested players yet... Be the first!", inline=False)
+            # discord.Embed(color=discord.Color.green(), title=message, timestamp=datetime.now()) \
+            #     .set_footer(text="Minecraft Looking For Group Bot") \
+            #     .set_author(name=ctx.author.display_name, icon_url=ctx.author.display_avatar) \
+            #     .set_thumbnail(url="https://static.wikia.nocookie.net/minecraft_gamepedia/images/a/a4/Grass_Block_%28item%29_BE5.png/revision/latest?cb=20200901112517") \
+            #     .add_field(name="Interested Players:", value="No interested players yet... Be the first!", inline=False)
         plan_view = planview.PlanView(ctx.author.id, embed=embed, timeout=None)
         view_message = await ctx.send(view=plan_view, embed=embed)
         plan_view.set_message(view_message)
 
         await plan_view.wait()
         # Only way I could figure out how to ping people, not ideal. Ideal would be doing it
-        # inside of the PlanView class
+        # inside of the PlanView class, but it seems that you can't ping people inside of 
+        # an embed
         if plan_view.interested_players:
             await ctx.send(content=plan_view.interested_players_string, delete_after=0.1)
 
--- minecraftbot.py
+++ minecraftbot.py
@@ -1,5 +1,12 @@
+from discord import Embed
+from discord import Color
+from discord import File
 from discord.ext import commands
+
 from mcstatus import JavaServer
+from datetime import datetime
+import base64
+import logging
 
 class MinecraftBot(commands.Bot):
     def __init__(self, 
@@ -7,10 +14,47 @@ class MinecraftBot(commands.Bot):
                 #  minecraft_java_server=None,
                  **kwargs):
         super().__init__(**kwargs)
-        self.SERVER_UNSET_MESSAGE = "No Minecraft server has been set yet!"
-        self.SERVER_CONNECT_FAIL_MESSAGE = "Could not connect to that address's Minecraft server!"
+        self._SERVER_UNSET_MESSAGE = "No Minecraft server has been set yet!"
+        self._SERVER_CONNECT_FAIL_MESSAGE = "Could not connect to that address's Minecraft server!"
         self.minecraft_java_server: JavaServer = None
+
+        # Fill in `timestamp`, author information, title
+        self._template_embed = \
+            Embed(color=Color.green(), title="Insert text here") \
+                .set_footer(text="Minecraft Looking For Group Bot") \
+                .set_thumbnail(url="https://static.wikia.nocookie.net/minecraft_gamepedia/images/a/a4/Grass_Block_%28item%29_BE5.png/revision/latest?cb=20200901112517")
+                # .add_field(name="Interested Players:", value="No interested players yet... Be the first!", inline=False)
+        
+        self.server_icon: File = self.update_icon('')
+        self.SERVER_UNSET_EMBED = \
+            self.template_embed(title=self._SERVER_UNSET_MESSAGE) \
+            .set_thumbnail(url="https://static.wikia.nocookie.net/minecraft_gamepedia/images/9/9e/Barrier_%28held%29_JE2_BE2.png/revision/latest?cb=20221216145252")
+        self.SERVER_CONNECT_FAIL_EMBED = \
+            self.template_embed(title=self._SERVER_CONNECT_FAIL_MESSAGE) \
+            .set_thumbnail(url="https://static.wikia.nocookie.net/minecraft_gamepedia/images/9/9e/Barrier_%28held%29_JE2_BE2.png/revision/latest?cb=20221216145252")
+        
     
+    def template_embed(self, *, title="Insert text here") -> Embed:
+        rv = self._template_embed.copy()
+        rv.timestamp = datetime.now()
+        rv.title = title
+        return rv
+    
+    def update_icon(self, filename: str) -> File:
+        try:
+            decoded = base64.b64decode(self.minecraft_java_server.status().icon.removeprefix("data:image/png;base64,"))
+            with open("server-icon.png", "wb") as write_file:
+                write_file.write(decoded)
+            read_file = open("server-icon.png", "rb")
+            self.server_icon = File(read_file, filename=filename)
+            read_file.close()
+        except:
+            # logging.error("Error creating server icon! Defaulting to question mark...")
+            default_file = open("default-icon.png", "rb")
+            self.server_icon = File(default_file)
+            default_file.close()
+        return self.server_icon
+
     def server_is_set(self) -> bool:
         return self.minecraft_java_server
 
@@ -30,4 +74,4 @@ class MinecraftBot(commands.Bot):
         MUST check `server_is_online()` before calling this function.
         """
         address = self.minecraft_java_server.address
-        return f"{address.host.split('_tcp.')[-1]}:{address.port}"
+        return f"{address.host.split('_tcp.')[-1]}{'' if address.port == 25565 else ':' + str(address.port)}"
